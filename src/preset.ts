30f08e92b0d81d77c4448087c24fdb5abbdfc953
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-08-18 13:54:08 +0200

    feat: Support injecting local swcrc file into default config
    BREAKING CHANGE: Your swcrc file is now taken into account, so
    your default SWC config may differ from what it previously was.

--- src/preset.ts
+++ src/preset.ts
@@ -1,3 +1,6 @@
+import path from "node:path";
+import fs from "node:fs";
+
 import { getProjectRoot } from "storybook/internal/common";
 import type { Options } from "storybook/internal/types";
 import type { Options as SwcOptions } from "@swc/core";
@@ -8,24 +11,49 @@ const virtualModuleFiles = [
   /storybook-stories\.js$/,
 ];
 
+const root = getProjectRoot();
+
+function findSWCConfigFile() {
+  const potentialPaths = [
+    // @ts-expect-error TS4111
+    process.env.SWC_CONFIG_PATH ?? "",
+    path.join(process.cwd(), ".swcrc"),
+    path.join(root, ".swcrc"),
+  ];
+
+  return potentialPaths.find((filePath) => fs.existsSync(filePath)) ?? null;
+}
+
+const swcConfigPath: string | null = findSWCConfigFile();
+
 export const webpackFinal = async (config: Configuration, options: Options) => {
+  const swcConfigFileOptions: SwcOptions = swcConfigPath
+    ? JSON.parse(fs.readFileSync(swcConfigPath, "utf-8"))
+    : {};
+
   const swcDefaultOptions: SwcOptions = {
+    ...(swcConfigFileOptions ?? {}),
     env: {
       // Transpiles the broken syntax to the closest non-broken modern syntax.
       // E.g. it won't transpile parameter destructuring in Safari
       // which would break how we detect if the mount context property is used in the play function.
       bugfixes: true,
+      ...(swcConfigFileOptions.env ?? {}),
     },
     jsc: {
+      ...(swcConfigFileOptions.jsc ?? {}),
       transform: {
         react: {
           runtime: "automatic",
+          ...(swcConfigFileOptions.jsc?.transform?.react ?? {}),
         },
+        ...(swcConfigFileOptions.jsc?.transform ?? {}),
       },
       parser: {
-        syntax: "typescript",
         tsx: true,
         dynamicImport: true,
+        ...(swcConfigFileOptions.jsc?.parser ?? {}),
+        syntax: swcConfigFileOptions.jsc?.parser?.syntax ?? "typescript",
       },
     },
   };
@@ -56,7 +84,7 @@ export const webpackFinal = async (config: Configuration, options: Options) => {
             options: swcOptions,
           },
         ],
-        include: [getProjectRoot()],
+        include: [root],
         exclude: [/node_modules/, ...virtualModuleFiles],
       },
     ],
